d6f4c5e8c191f1416498e16bbf2cda05ea36e6f1
Daniel Chudnov <dchud@gwu.edu>
2013-01-07 17:20:32 -0500

    more basics

--- loggins/loggins/settings.py
+++ loggins/loggins/settings.py
@@ -1,4 +1,4 @@
-# Django settings for loggins project.
+from os.path import abspath, dirname
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -24,7 +24,7 @@ DATABASES = {
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = dirname(dirname(abspath(__file__))) + '/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -115,10 +115,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'tastypie',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -149,3 +147,10 @@ LOGGING = {
         },
     }
 }
+
+
+# Be sure to create your own 'local_settings.py' file as described in README
+try:
+    from local_settings import *
+except ImportError:
+    pass
