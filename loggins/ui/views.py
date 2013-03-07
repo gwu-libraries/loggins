841edb41bb9b6c2747535240ed919d6fc7aa12d5
Daniel Chudnov <dchud@gwu.edu>
2013-03-06 23:02:13 -0500

    more useful home page, or at least a start, refs #7

--- loggins/ui/views.py
+++ loggins/ui/views.py
@@ -1,7 +1,8 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db import connection
 from django.shortcuts import get_object_or_404, render
 
-from ui.models import Host, Record
+from ui.models import Host
 
 
 def _paginate(request, paginator):
@@ -16,26 +17,50 @@ def _paginate(request, paginator):
 
 
 def home(request):
-    qs_records = Record.objects.order_by('-timestamp')
-    paginator = Paginator(qs_records, 50)
-    page, records = _paginate(request, paginator)
-    state = Record.objects.raw('''
-        SELECT *
-        FROM ui_record r1, ui_host
-        WHERE timestamp=(
-            SELECT MAX(timestamp)
-            FROM ui_record
-            WHERE host_id=r1.host_id
-            AND location != ''
-        )
-        AND ui_host.id = r1.host_id
-        AND ui_host.is_active IS true
-        ORDER BY location''')
-
+    cursor = connection.cursor()
+    cursor.execute('''
+        SELECT COUNT(*), 
+            SUBSTRING(ui_host.location FROM 1 FOR 1) AS library, 
+            SUBSTRING(ui_host.location FROM 2 FOR 1) AS floor, event
+            FROM ui_record r1, ui_host        
+            WHERE timestamp=(
+                SELECT MAX(timestamp)
+                FROM ui_record
+                WHERE host_id=r1.host_id
+                AND location != ''
+            )
+            AND ui_host.id = r1.host_id
+            AND ui_host.is_active IS true 
+            GROUP BY library, floor, event 
+            ORDER BY library, floor, event;
+        ''')
+    codes = {}
+    # presuming a small dataset; we can get away with fetchall()
+    for row in cursor.fetchall():
+        count, library, floor, event = row        
+        code = library + floor
+        try:
+            s = codes[code]
+        except:
+            s = {'code': code, 'floor': floor}
+        if library == 'g':
+            s['library'] = 'Gelman'
+        elif library == 'e':
+            s['library'] = 'Eckles'
+        elif library == 'v':
+            s['library'] = 'VSTC'
+        if event == 'i':
+            s['unavailable'] = count
+        elif event == 'o':
+            s['available'] = count
+        s['total'] = s.get('available', 0) + s.get('unavailable', 0)
+        codes[code] = s
+    all_floors = sorted([(code, s) for code, s in codes.items()])
+    floors = [floor for code, floor in all_floors]
     return render(request, 'home.html', {
         'title': 'home',
-        'records': records,
-        'state': state,
+        'codes': codes,
+        'floors': floors,
     })
 
 
