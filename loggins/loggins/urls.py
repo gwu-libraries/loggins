6f0f177271a8f3a74936a26f2f5d245ea4dc86f9
Dan Kerchner <kerchner@gwu.edu>
2013-06-20 19:17:56 -0400

    Fixes #27

--- loggins/loggins/urls.py
+++ loggins/loggins/urls.py
@@ -17,6 +17,10 @@ urlpatterns = patterns('',
 
 urlpatterns += patterns('ui.views',
     url(r'^$', 'home', name='home'),
-    url(r'^host/(?P<host_location>[a-z]\d{3})/$', 'host', name='host'),
+# Requires the numeric segment of the location to be 3 digits:
+#    url(r'^host/(?P<host_location>[a-z]\d{3})/$', 'host', name='host'),
+#
+# Allows the numeric portion to be any number of digits (1 or more):
+    url(r'^host/(?P<host_location>[a-z]\d+)/$', 'host', name='host'),
     url(r'^floor/(?P<code>[a-z][0-9])/$', 'floor', name='floor'),
 )
