2e1299545b49c37067f2d39c399b38f2f273304c
Darshan Pandhi <darshan@email.gwu.edu>
2013-07-30 12:18:49 -0400

    Modified to read the community string from the settings file. ref #60

--- loggins/ui/management/commands/capture_snmp_state.py
+++ loggins/ui/management/commands/capture_snmp_state.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from django.utils.timezone import utc
 from django.core.management.base import BaseCommand
+from django.conf import settings
 
 from ui.models import Location
 
@@ -18,7 +19,7 @@ class Command(BaseCommand):
 
         for location in locations:
             errorIndication, errorStatus, errorIndex, varBindTable = cmdGen.nextCmd(
-                cmdgen.CommunityData('public'), cmdgen.UdpTransportTarget(
+                cmdgen.CommunityData(settings.SNMP_COMMUNITY_STRING), cmdgen.UdpTransportTarget(
                     (location.ip_address, 161)),
                     '1.3.6.1.4.1.25071.1.2.6.1.1.2',
                     '1.3.6.1.4.1.25071.1.1.2.1.1.3',)
