591bb60503105521aa83fe5a57d12b2f898769dc
kerchner <kerchner@gwu.edu>
2013-12-02 13:44:47 -0500

    Added creation of GW Libraries buildings, floors, zones prior to migration of existing Locations

--- loggins/ui/migrations/0005_floors_zones.py
+++ loggins/ui/migrations/0005_floors_zones.py
@@ -8,6 +8,81 @@ from django.db import models
 class Migration(DataMigration):
 
     def forwards(self, orm):
+        # ---- POPULATE WITH NEW GWLIBRARY BUILDINGS, FLOORS, ZONES ----
+        building_gelman = orm.Building(name='Gelman')
+        building_gelman.save()
+        building_eckles = orm.Building(name='Eckles')
+        building_eckles.save()
+        building_vstc = orm.Building(name='VSTC')
+        building_vstc.save()
+
+        floor_gelman0 = orm.Floor(floor=0, building=building_gelman)
+        floor_gelman0.save()
+        floor_gelman1 = orm.Floor(floor=1, building=building_gelman)
+        floor_gelman1.save()
+        floor_gelman2 = orm.Floor(floor=2, building=building_gelman)
+        floor_gelman2.save()
+        floor_gelman3 = orm.Floor(floor=3, building=building_gelman)
+        floor_gelman3.save()
+        floor_gelman4 = orm.Floor(floor=4, building=building_gelman)
+        floor_gelman4.save()
+        floor_gelman5 = orm.Floor(floor=5, building=building_gelman)
+        floor_gelman5.save()
+        floor_gelman6 = orm.Floor(floor=6, building=building_gelman)
+        floor_gelman6.save()
+        floor_gelman7 = orm.Floor(floor=7, building=building_gelman)
+        floor_gelman7.save()
+        floor_eckles1 = orm.Floor(floor=1, building=building_eckles)
+        floor_eckles1.save()
+        floor_eckles2 = orm.Floor(floor=2, building=building_eckles)
+        floor_eckles2.save()
+        floor_vstc1 = orm.Floor(floor=1, building=building_vstc)
+        floor_vstc1.save()
+
+        zone_gelmanll = orm.Zone(floor=floor_gelman0, name='Lower Level',
+                                 display_order=0)
+        zone_gelmanll.save()
+        zone_gelman1 = orm.Zone(floor=floor_gelman1, name='1st Floor',
+                                display_order=1)
+        zone_gelman1.save()
+        zone_gelmanentrance = orm.Zone(floor=floor_gelman2,
+                                       name='Entrance Floor',
+                                       display_order=2)
+        zone_gelmanentrance.save()
+        zone_gelmanlab = orm.Zone(floor=floor_gelman2,
+                                  name='Lab @ Entrance Floor',
+                                  display_order=3)
+        zone_gelmanlab.save()
+        zone_gelman3 = orm.Zone(floor=floor_gelman3, name='3rd Floor',
+                                display_order=4)
+        zone_gelman3.save()
+        zone_gelman4 = orm.Zone(floor=floor_gelman4, name='4th Floor',
+                                display_order=5)
+        zone_gelman4.save()
+        zone_gelman5 = orm.Zone(floor=floor_gelman5, name='5th Floor',
+                                display_order=6)
+        zone_gelman5.save()
+        zone_gelman6 = orm.Zone(floor=floor_gelman6, name='6th Floor',
+                                display_order=7)
+        zone_gelman6.save()
+        zone_gelmangrc = orm.Zone(floor=floor_gelman6,
+                                  name='Global Resources Center',
+                                  display_order=8)
+        zone_gelmangrc.save()
+        zone_gelman7 = orm.Zone(floor=floor_gelman7, name='7th Floor',
+                                display_order=9)
+        zone_gelman7.save()
+        zone_eckles1 = orm.Zone(floor=floor_eckles1, name='Eckles Main',
+                                display_order=0)
+        zone_eckles1.save()
+        zone_eckles2 = orm.Zone(floor=floor_eckles2, name='Eckles 2nd Floor',
+                                display_order=1)
+        zone_eckles2.save()
+        zone_vstc1 = orm.Zone(floor=floor_vstc1, name='VSTC Main',
+                              display_order=0)
+        zone_vstc1.save()
+
+        # ---- MAP OLD LOCATION DATA TO NEW OBJECTS ----
         # try to find the entrance floor lab zone and
         # entrance floor non-lab zone
         labzone = orm.Zone.objects.get(floor__building__name='Gelman',
